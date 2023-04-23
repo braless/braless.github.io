3e81b236d08680dee06c272084f87de819d1dc2b
Linux <braless@linux.com>
2023-04-23 15:49:19 +0800

    update repo

--- update.sh
+++ update.sh
@@ -1,24 +1,12 @@
 #!/bin/sh
-dpkg-scanpackages --multiversion root > Packages
-dpkg-scanpackages --multiversion rootless >> Packages
+echo("开始生成Pakages...")
 
-cat Packages | xz > Packages.xz
-cat Packages | bzip2 > Packages.bz2
-cat Packages | gzip > Packages.gz
-cat Packages | lzma > Packages.lzma
-cat Packages | zstd > Packages.zst
+python3 dpkg-scanpackages.py -m debs/ > Packages
+echo("生成Packages完成！")
+echo("开始压缩Packages...")
 
-apt-ftparchive\
- -o APT::FTPArchive::Release::Origin="DaoDao's Repo"\
- -o APT::FTPArchive::Release::Label="DaoDao's Repo"\
- -o APT::FTPArchive::Release::Suite="stable"\
- -o APT::FTPArchive::Release::Version="1.0"\
- -o APT::FTPArchive::Release::Codename="ios"\
- -o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64"\
- -o APT::FTPArchive::Release::Components="main"\
- -o APT::FTPArchive::Release::Description="DaoDao's tweak repository"\
- release . > Release
-
+bzip2  -f -k  Packages
+echo("压缩Packages完成！")
 git add .
 git commit -m "update repo"
 git push
