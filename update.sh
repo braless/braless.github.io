9e500bd745baa211fb52b92c86dc3925d2ffa5b8
Linux <braless@linux.com>
2023-04-23 15:54:18 +0800

    update repo

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#echo("开始生成Pakages...")
+echo "开始生成Pakages..."
 python3 dpkg-scanpackages.py -m debs/ > Packages
 
 
-#echo("生成Packages完成！")
-#echo("开始压缩Packages...")
+echo "生成Packages完成！"
+echo "开始压缩Packages..."
 
 bzip2  -f -k  Packages
-#echo("压缩Packages完成！")
+echo "压缩Packages完成！"
 git add .
 git commit -m "update repo"
 git push
