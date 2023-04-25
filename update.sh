55f6431cc486c5a4f22040bc0a84ed8b0f19f45a
Linux <braless@linux.com>
2023-04-25 22:45:41 +0800

    update repo

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "开始生成Pakages..."
-python3 dpkg-scanpackages.py -m debs > Packages
+python3 /APP-JD/braless.github.io/dpkg-scanpackages.py -m /APP-JD/braless.github.io/debs > /APP-JD/braless.github.io/Packages
 
 
 echo "生成Packages完成！"
@@ -8,6 +8,9 @@ echo "开始压缩Packages..."
 
 bzip2  -f -k  Packages
 echo "压缩Packages完成！"
+
+cd /APP-JD/braless.github.io/
+echo "进入项目目录..."
 git add .
 git commit -m "update repo"
 git push
