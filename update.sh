14070ccc8ebd05260bef9e2478d98190347e5533
Linux <braless@linux.com>
2023-11-23 15:01:29 +0800

    update repo

--- update.sh
+++ update.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+
+#发送通知..
+TOKEN="6222369978:AAE7hkZEJhuU0KFgzdLziX-Y-GN_mn4Ucxk"	#TG机器人token
+chat_ID=275642085		#用户ID或频道、群ID
+message_text="越狱软件源.检测到更新..正在推送到Github"		#要发送的信息
+MODE='Markdown'		#解析模式，可选HTML或Markdown
+URL="https://tg.bilin.eu.org/bot${TOKEN}/sendMessage"		#api接口
+#测试1：终端有日志
+#curl -s -X POST $URL -d chat_id=${chat_ID}  -d parse_mode=${MODE} -d text="${message_text}"  	
+#测试2：终端无日志
+curl -s -o /dev/null -X POST $URL -d chat_id=${chat_ID} -d text="${message_text}"  
+
+
+#压缩打包
 cd /APP-JD/braless.github.io/
 echo "开始生成Pakages..."
 python3 dpkg-scanpackages.py -m debs > Packages
@@ -12,3 +26,6 @@ git add --all .
 git commit -m "update repo"
 git push
 echo "上传完成！！"
+
+
+
